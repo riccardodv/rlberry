eb67d255626461c0e2e251d346b7febbd30fe93f
riccardo <riccardo.dellavecchia91@gmail.com>
2022-03-02 11:02:47 +0100

    update video_plot_SAC.py

--- examples/demo_agents/video_plot_SAC.py
+++ examples/demo_agents/video_plot_SAC.py
@@ -1,32 +1,21 @@
 """
-=============================
-Record reward during training
-=============================
 
-This script shows how to modify an agent to easily record reward or action
-during the fit of the agent.
 """
 
-
-# import numpy as np
-# from rlberry.wrappers import WriterWrapper
-from rlberry.envs.basewrapper import Wrapper
-
-# from rlberry.envs import gym_make
 from rlberry.manager import plot_writer_data, AgentManager
 from rlberry.envs.benchmarks.ball_exploration import PBall2D
 from rlberry.agents.torch import SACAgent
 import gym
 
 
-# we dont need wrapper actually just 'return env' works
-def env_ctor(env, wrap_spaces=True):
-    return Wrapper(env, wrap_spaces)
+def env_ctor():
+    env = PBall2D()
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
+    return env
 
 
-env = PBall2D()
-env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
-env_kwargs = dict(env=env)
+env_kwargs = dict()
+
 agent = AgentManager(
     SACAgent,
     (env_ctor, env_kwargs),
@@ -34,26 +23,6 @@ agent = AgentManager(
     n_fit=1,
     enable_tensorboard=True,
 )
-
-# basic version
-# env_kwargs = dict(id = "CartPole-v0")
-# agent = AgentManager(SACAgent, (gym_make, env_kwargs), fit_budget=200, n_fit=1)
-
-# # timothe's
-# env = gym_make("CartPole-v0")
-# agent = AgentManager(
-#     SACAgent, (env.__class__, dict()), fit_budget=200, n_fit=1, enable_tensorboard=True,
-# )
-
-# Omar's
-# env = gym_make("CartPole-v0")
-# from copy import deepcopy
-# def env_constructor():
-#     return deepcopy(env)
-# agent = AgentManager(
-#     SACAgent, (env_constructor, dict()), fit_budget=200, n_fit=1, enable_tensorboard=True,
-# )
-
 
 agent.fit()
 
